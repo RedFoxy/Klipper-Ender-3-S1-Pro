bf0b98be3fed1e236ef7fee66af572b1332b9867
RedFoxy Darrest <redfoxy@redfoxy.it>
2024-02-09 16:38:46 +0100

    First release

---
+++ scripts/turn_off_webcam.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+sudo systemctl stop camera-streamer-generic-usb-cam.service
+#sudo systemctl stop webcamd.service
+#uhubctl -l 1-1 -p 2 -a off
+
---
+++ scripts/turn_on_webcam.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+sudo systemctl start camera-streamer-generic-usb-cam.service
+#sudo systemctl start webcamd.service
+#uhubctl -l 1-1 -p 2 -a on
+
